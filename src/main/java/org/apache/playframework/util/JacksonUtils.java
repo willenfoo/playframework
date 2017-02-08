8e17881b415104ee729ace7fef060c91f363665c
willenfoo <willenfoo@gmail.com>
2017-02-08 09:36:00 +0800

    1

--- src/main/java/org/apache/playframework/util/JacksonUtils.java
+++ src/main/java/org/apache/playframework/util/JacksonUtils.java
@@ -1,10 +1,11 @@
 package org.apache.playframework.util;
 
 import java.text.SimpleDateFormat;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
  
 
 public class JacksonUtils {
@@ -12,9 +13,15 @@ public class JacksonUtils {
 	private static final Log log = LogFactory.getLog(JacksonUtils.class);
 	
 	public static String writeValueAsString(Object object) {
-		 
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 		String result = null;
-		 
+		try {
+			result = mapper.writeValueAsString(object);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.debug("返回的json数据是===========" + result);
 		return result;
 	}
 	
