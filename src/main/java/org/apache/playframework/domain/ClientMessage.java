fd43814a9c785332cac4a20781e3f989b2ba5e23
willenfoo <willenfoo@gmail.com>
2017-02-21 11:44:29 +0800

    添加客户端返回消息

---
+++ src/main/java/org/apache/playframework/domain/ClientMessage.java
@@ -0,0 +1,93 @@
+package org.apache.playframework.domain;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.playframework.util.JacksonUtils;
+
+/**
+ * 客户端报文
+ * 
+ * @author tangxiaojun
+ *
+ */
+public class ClientMessage {
+
+	// 错误描述信息
+	private String msg = "操作成功";
+	 
+	// 对象信息
+	private Object data = CollectionUtils.EMPTY_COLLECTION;
+
+	/**
+	 * 1000 : 访问正常 10001：当前接口弃用需要客户端强制升级 1002：维护中 (提示消息放入errorMsg字段中)
+	 * 1003：当前访问的接口有新版本可使用 1004： jsession失效 1005：接口异常或错误
+	 */
+	private Integer code = 1000;
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public ClientMessage() {
+		
+	}
+	
+	public ClientMessage(Object data) {
+		this.data = data;
+	}
+	
+	public ClientMessage(Integer code, String msg) {
+		this.msg = msg;
+		this.code = code;
+	}
+
+	public ClientMessage(Object data, String msg) {
+		this.msg = msg;
+		this.data = data;
+	}
+
+	public static final ClientMessage success() {
+		return new ClientMessage();
+	}
+
+	public static final ClientMessage success(Object data) {
+		return new ClientMessage(data);
+	}
+
+	public static final ClientMessage success(Object data, String msg) {
+		return new ClientMessage(data, msg);
+	}
+
+	public static final ClientMessage failed(String msg) {
+		return new ClientMessage(1005, msg);
+	}
+
+	public static final ClientMessage failed(Integer code, String msg) {
+		return new ClientMessage(code, msg);
+	}
+ 
+	
+	public static void main(String[] args) {
+		ClientMessage cm = success();
+		System.out.println(JacksonUtils.writeValueAsString(cm));
+	}
+}
