94114c2ed987608fe733deb1df42f0b3b25995c2
willenfoo <willenfoo@foxmail.com>
2018-09-13 15:39:00 +0800

    框架重新整理

---
+++ src/main/java/org/apache/playframework/exception/GlobalExceptionHandler.java
@@ -0,0 +1,103 @@
+package org.apache.playframework.exception;
+
+
+import com.baomidou.mybatisplus.extension.api.IErrorCode;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.enums.ApiErrorCode;
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.HandlerMethod;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: fuwei
+ * @date: 2018/8/26
+ * @time: 13:37
+ * @description: 通用 Api Controller 全局异常处理
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * <p>
+     * 自定义 REST 业务异常
+     * <p>
+     *
+     * @param e 异常类型
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    public R<Object> handleBadRequest(HandlerMethod handlerMethod, Exception e) {
+
+        Object resultData = null;
+        Type genericReturnType = handlerMethod.getMethod().getGenericReturnType();
+        //获取返回值的泛型参数
+        if(genericReturnType instanceof ParameterizedType){
+            Type[] actualTypeArguments = ((ParameterizedType)genericReturnType).getActualTypeArguments();
+            for (Type type : actualTypeArguments) {
+                if (type instanceof ParameterizedTypeImpl) {
+                    ParameterizedTypeImpl parameterizedType = (ParameterizedTypeImpl)type;
+                    if (parameterizedType.getRawType().getName().equals(List.class.getName())) {
+                        resultData = new ArrayList<>();
+                    } else {
+                        resultData = new Object();
+                    }
+                    break;
+                } else {
+                    resultData = new Object();
+                    break;
+                }
+            }
+        }
+
+        /*
+         * 业务逻辑异常
+         */
+        if (e instanceof ApiException) {
+            IErrorCode errorCode = ((ApiException) e).getErrorCode();
+            if (null != errorCode) {
+                logger.info("Rest request error, {}", errorCode.toString());
+                return R.restResult(resultData, errorCode);
+            }
+            logger.info("Rest request error, {}", e.getMessage());
+            return R.restResult(resultData, ApiErrorCode.FAILED);
+        }
+
+        /*
+         * 参数校验异常
+         */
+        if (e instanceof BindException) {
+            BindingResult bindingResult = ((BindException) e).getBindingResult();
+            if (null != bindingResult && bindingResult.hasErrors()) {
+                List<Object> jsonList = new ArrayList<>();
+                bindingResult.getFieldErrors().stream().forEach(fieldError -> {
+                    Map<String, Object> jsonObject = new HashMap<>(2);
+                    jsonObject.put("name", fieldError.getField());
+                    jsonObject.put("msg", fieldError.getDefaultMessage());
+                    jsonList.add(jsonObject);
+                });
+                return R.restResult(jsonList, ApiErrorCode.FAILED);
+            }
+        }
+
+        /**
+         * 系统内部异常，打印异常栈
+         */
+        logger.error("Error: handleBadRequest StackTrace : {}", e);
+        return R.restResult(resultData, ApiErrorCode.FAILED);
+    }
+}
